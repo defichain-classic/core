1c4011739fa47df687e0f204e1f79df11341774f
Defichain Classic Team <opensource@defichain-classic.com>
2023-12-15 16:27:37 +1000

    gaslimit

--- params/genesis_defichain.go
+++ params/genesis_defichain.go
@@ -13,7 +13,7 @@ func DefaultDefiChainGenesisBlock() *genesisT.Genesis {
 		Config:     DefiChainConfig,
 		Nonce:      hexutil.MustDecodeUint64("0x0"),
 		ExtraData:  hexutil.MustDecode("0x42"),
-		GasLimit:   hexutil.MustDecodeUint64("0x2fefd8"),
+		GasLimit:   hexutil.MustDecodeUint64("0x2fefd80"),
 		Difficulty: hexutil.MustDecodeBig("0x2000"),
 		Timestamp:  1623664673,
 		Alloc:      genesisT.DecodePreAlloc(allocDefiChain),
